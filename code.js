f310c1c975d4b196f865b6e5bd2af559456e1451
NekroReal <eraridane42@gmail.com>
2023-06-17 16:50:39 +0100

    el jueguito completo

--- code.js
+++ code.js
@@ -1,1 +1,50 @@
-console.log ("Hello World");
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+let xs = prompt("Piedra, papel o tijeras")
+function piedraPapelTijeras() {
+    /** el codigo juega a piedra papel y tijeras */
+    switch (randomNumber) {
+        case 1:
+            console.log("la maquina tiro papel y el jugador tiro " + xs)
+            if (xs == "papel") {
+                console.log("empate");
+            }
+            else if (xs == "piedra") {
+                console.log("perdiste");
+            }
+            else if (xs == "tijeras") {
+                console.log("ganaste");
+            }
+            break;
+
+        case 2:
+            console.log("la maquina tiro piedra y el jugador tiro " + xs)
+            if (xs == "piedra") {
+                console.log("empate");
+            }
+            else if (xs == "tijeras") {
+                console.log("perdiste");
+            }
+            else if (xs == "papel") {
+                console.log("ganaste");
+            }
+            break;
+
+        case 3:
+            console.log("la maquina tiro tijeras y el jugador tiro " + xs)
+            if (xs == "tijeras") {
+                console.log("empate");
+            }
+            else if (xs == "papel") {
+                console.log("perdiste");
+            }
+            else if (xs == "piedra") {
+                console.log("ganaste");
+            }
+            break;
+
+        default:
+            break;
+    }
+
+}
+piedraPapelTijeras()
